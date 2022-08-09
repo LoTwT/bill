d123f5727ad0eb8ae729ef34fecf9b2c48e8b425
LoTwT <709937065@qq.com>
2022-08-09 20:52:32 +0800

    feat(server): login api

--- packages/server/app/controller/user.js
+++ packages/server/app/controller/user.js
@@ -58,6 +58,54 @@ class UserController extends Controller {
       }
     }
   }
+
+  async login() {
+    const { ctx, app } = this
+    const { username, password } = ctx.request.body
+
+    // 根据用户名，在数据库查找相对应的 id 操作
+    const userInfo = await ctx.service.user.getUserByName(username)
+
+    // 没找到，说明该用户未注册
+    if (!userInfo || !userInfo.id) {
+      ctx.body = {
+        code: 500,
+        msg: "账号不存在",
+        data: null,
+      }
+
+      return
+    }
+
+    // 找到用户，判断密码
+    if (userInfo && password != userInfo.password) {
+      ctx.body = {
+        code: 500,
+        msg: "帐号密码错误",
+        data: null,
+      }
+
+      return
+    }
+
+    // 生成 token 并加盐
+    const token = app.jwt.sign(
+      {
+        id: userInfo.id,
+        username: userInfo.username,
+        exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60, // 有效期 24 小时
+      },
+      app.config.jwt.secret,
+    )
+
+    ctx.body = {
+      code: 200,
+      msg: "登录成功",
+      data: {
+        token,
+      },
+    }
+  }
 }
 
 module.exports = UserController
