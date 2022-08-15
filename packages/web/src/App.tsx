8d577fa7721cbb6f3c045f2812c1382b7c62efb5
LoTwT <709937065@qq.com>
2022-08-15 20:07:41 +0800

    chore(web): add zarm

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,18 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import routes from "./router/index"
 
+import { ConfigProvider } from "zarm"
+import "zarm/dist/zarm.css"
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<route.component />}
-          />
-        ))}
-      </Routes>
+      {/* @ts-ignore */}
+      <ConfigProvider primaryColor={"#007fff"}>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+      </ConfigProvider>
     </BrowserRouter>
   )
 }
--- packages/web/src/container/Index/index.tsx
+++ packages/web/src/container/Index/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "zarm"
+
 function Index() {
-  return <div>Index</div>
+  return (
+    <div>
+      Index
+      <Button theme="primary">按钮</Button>
+    </div>
+  )
 }
 
 export default Index
