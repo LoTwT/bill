bd1577b32514d51a4a0ad317e0a6af7a403d1cc7
LoTwT <709937065@qq.com>
2022-08-15 20:21:45 +0800

    chore(web): add less

--- packages/web/src/container/Index/index.tsx
+++ packages/web/src/container/Index/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "zarm"
+import style from "./style.module.less"
 
 function Index() {
   return (
-    <div>
-      Index
+    <div className={style.index}>
+      <span>样式</span>
       <Button theme="primary">按钮</Button>
     </div>
   )
