68279fa502d08a7dc7ab94c34f2d43295416cc1c
LoTwT <709937065@qq.com>
2022-08-16 11:19:23 +0800

    feat(web): Login add Captcha and base states

--- packages/web/src/container/Login/index.tsx
+++ packages/web/src/container/Login/index.tsx
@@ -1,9 +1,24 @@
 import { Cell, Input, Button, Checkbox } from "zarm"
 import CustomIcon from "@/components/CustomIcon"
 
+import Captcha from "react-captcha-code"
+
 import style from "./style.module.less"
+import { useCallback, useState } from "react"
 
 function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [verify, setVerify] = useState("")
+
+  // 验证码变化后的值
+  const [captcha, setCaptcha] = useState("")
+
+  const handleChange = useCallback((captcha: string) => {
+    console.log("captcha", captcha)
+    setCaptcha(captcha)
+  }, [])
+
   return (
     <div className={style.auth}>
       <div className={style.head} />
@@ -14,15 +29,31 @@ function Login() {
 
       <div className={style.form}>
         <Cell icon={<CustomIcon type="zhanghao" />}>
-          <Input clearable type="text" placeholder="请输入账号" />
+          <Input
+            clearable
+            type="text"
+            placeholder="请输入账号"
+            onChange={(value?: string) => setUsername(value!)}
+          />
         </Cell>
 
         <Cell icon={<CustomIcon type="mima" />}>
-          <Input clearable type="password" placeholder="请输入密码" />
+          <Input
+            clearable
+            type="password"
+            placeholder="请输入密码"
+            onChange={(value?: string) => setPassword(value!)}
+          />
         </Cell>
 
         <Cell icon={<CustomIcon type="mima" />}>
-          <Input clearable type="text" placeholder="请输入验证码" />
+          <Input
+            clearable
+            type="text"
+            placeholder="请输入验证码"
+            onChange={(value?: string) => setVerify(value!)}
+          />
+          <Captcha charNum={4} onChange={handleChange} />
         </Cell>
       </div>
 
