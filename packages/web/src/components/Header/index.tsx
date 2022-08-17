f9406d8f9e0cfc91859fdc76b3a00ccc50d2bcb9
LoTwT <709937065@qq.com>
2022-08-17 09:36:29 +0800

    feat(web): Detail Header UI

---
+++ packages/web/src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import s from "./style.module.less"
+import { useNavigate } from "react-router-dom"
+import { Icon, NavBar } from "zarm"
+
+interface IHeaderProps {
+  title?: string
+}
+
+function Header(props: IHeaderProps) {
+  const { title = "" } = props
+  console.log("====", title)
+  const navigate = useNavigate()
+
+  return (
+    <div className={s.headerWrap}>
+      <div className={s.block}>
+        <NavBar
+          className={s.header}
+          left={
+            <Icon
+              type="arrow-left"
+              theme="primary"
+              onClick={() => navigate(-1)}
+            />
+          }
+          title={title}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ packages/web/src/container/Detail/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header"
+import s from "./style.module.less"
+
+function Detail() {
+  return (
+    <div className={s.detail}>
+      <Header title="账单详情" />
+    </div>
+  )
+}
+
+export default Detail
